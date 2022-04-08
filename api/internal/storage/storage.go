6f3e8a9474c546f518e84d9774e512ba4b379a5f
Erlend Hårstad <erha@equinor.com>
2022-04-08 10:48:43 +0200

    Add a basic azblob implementaion of StorageClient
    This commit introduces an azure blobstore specific implementation of the
    storage facade StorageClient. It's very simple at the moment, with no
    support for caching and only support credentials in the form of
    query-parameters. Once it's a bit more mature the rest of the system
    (query api and fetch) will adopt and use this implementation.

--- api/internal/storage/storage.go
+++ api/internal/storage/storage.go
@@ -3,6 +3,11 @@ package storage
 import (
 	"context"
 	"net/url"
+	"io/ioutil"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+
+	"github.com/equinor/oneseismic/api/internal"
 )
 
 /*
@@ -16,4 +21,34 @@ type StorageClient interface {
 	 * Get a blob or file from storage
 	 */
 	Get(ctx context.Context, bloburl *url.URL) ([]byte, error)
+}
+
+/*
+ * Azure Blob Store implementation of a oneseismic StorageClient
+ */
+type AzStorage struct {
+}
+
+func (c *AzStorage) Get(ctx context.Context, bloburl *url.URL) ([]byte, error) {
+	if bloburl == nil {
+		return []byte{}, internal.InternalError("blob URL is nil")
+	}
+
+	client, err := azblob.NewBlobClientWithNoCredential(bloburl.String(), nil)
+	if err != nil {
+		return []byte{}, internal.InternalError(err.Error())
+	}
+
+	dl, err := client.Download(ctx, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	body := dl.Body(&azblob.RetryReaderOptions{})
+	defer body.Close()
+	return ioutil.ReadAll(body)
+}
+
+func NewAzClient() *AzStorage {
+	return &AzStorage{}
 }
